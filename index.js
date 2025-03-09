5f1aceca4684ee5febbeb0ccfb12a551570075a5
Donald-27 <+>
2025-03-09 20:19:11 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// Function to create thank-you cards
+function writeCards(names, event) {
+    let messages = []; // Create an empty array to store messages
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages; // Return the array of messages
+  }
+  
+  // Function to count down from a given number to 0
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--; // Decrement the number each iteration
+    }
+  }
+  
+  // Exporting functions for testing
+  module.exports = { writeCards, countDown };
+  
